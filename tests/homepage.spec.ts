a342c79394d9d18c27374cf74f76cb9206a1d6e9
allania7med11 <allania7med11@gmail.com>
2024-10-27 19:42:32 +0000

    SHP2410211__spearate mock categoriesApiRoutes from the test

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,50 +1,13 @@
 import { test, expect } from '@playwright/test';
-import categoriesResponse from '../mocks/categoriesResponse.json';
+import { categoriesApiRoutes } from '../mockRoutes/categoriesApiRoutes.ts';
 
 test('The homepage should return a 200 status and verify that the categories render correctly.', async ({ page }) => {
   let categoriesRequested = false;
-
-  await page.route('**/api/**', (route, request) => {
-    const url = request.url();
-    const method = request.method();
-
-    if (url.includes('/api/auth/profile/')) {
-      route.fulfill({
-        status: 403,
-        contentType: 'application/json',
-        body: JSON.stringify({ message: 'Forbidden' }),
-      });
-    } else if (url.includes('/api/cart/current/')) {
-      route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({
-          id: 37,
-          total_amount: '0.00',
-          items: [],
-          address: null,
-        }),
-      });
-    } else if (url.includes('/api/categories/')) {
-      console.log('Request to /api/categories/ detected');
-      categoriesRequested = true;
-      route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify(categoriesResponse),
-      });
-    } else if (url.includes('/api/auth/csrf/')) {
-      console.log('Request to /api/auth/csrf/ detected');
-      route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({ csrfToken: 'dummy-csrf-token' }),
-      });
-    } else {
-      route.continue();
-    }
-  });
-
+  // Define a callback to set the categoriesRequested flag
+  const onCategoriesRequest = () => {
+    categoriesRequested = true;
+  };
+  await categoriesApiRoutes(page, onCategoriesRequest);
   const response = await page.goto('');
   await page.waitForLoadState('load');
   await page.waitForTimeout(2000);
